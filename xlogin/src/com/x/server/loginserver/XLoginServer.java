4d612664af88601a4f5f2284067a85e8944e55ec
dysonbird <dysonbird@gmail.com>
2014-02-18 18:28:50 +0800

    新增属性

--- xlogin/src/com/x/server/loginserver/XLoginServer.java
+++ xlogin/src/com/x/server/loginserver/XLoginServer.java
@@ -6,10 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.x.server.factory.LoginBeanFactory;
+import com.x.util.Common;
 
 public class XLoginServer {
 	private static Logger logger = LoggerFactory.getLogger(XLoginServer.class);
 
+	private boolean isMainServer = true;//是否是主登陆服务器,是的话要启动数据中心
+	private int serverPort = 20000;//供客户端连接的端口
+	private int maxHandleThread = 10;
+	private int maxFilterThread = 10; 
+	
+	private int serverPortHttp = 8079;//供客户端连接的端口
+	
+	private int platformType = Common.PLATFORM_TYPE_SELF;//平台类型
+	
 	/**
 	 * @param args
 	 */
@@ -30,4 +40,53 @@ public class XLoginServer {
 	public void init(){}
 	
 	public void serverStart(){}
+
+	public boolean isMainServer() {
+		return isMainServer;
+	}
+
+	public void setMainServer(boolean isMainServer) {
+		this.isMainServer = isMainServer;
+	}
+
+	public int getServerPort() {
+		return serverPort;
+	}
+
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
+
+	public int getMaxHandleThread() {
+		return maxHandleThread;
+	}
+
+	public void setMaxHandleThread(int maxHandleThread) {
+		this.maxHandleThread = maxHandleThread;
+	}
+
+	public int getMaxFilterThread() {
+		return maxFilterThread;
+	}
+
+	public void setMaxFilterThread(int maxFilterThread) {
+		this.maxFilterThread = maxFilterThread;
+	}
+
+	public int getServerPortHttp() {
+		return serverPortHttp;
+	}
+
+	public void setServerPortHttp(int serverPortHttp) {
+		this.serverPortHttp = serverPortHttp;
+	}
+
+	public int getPlatformType() {
+		return platformType;
+	}
+
+	public void setPlatformType(int platformType) {
+		this.platformType = platformType;
+	}
+	
 }
